64d691fa870bf99ac4174b1be2f36ebd81f63f27
bishoyfawzy <100249627+bishoyfawzy@users.noreply.github.com>
2026-04-17 01:21:02 -0400

    Update app.py

--- Website Development/app.py
+++ Website Development/app.py
@@ -66,7 +66,7 @@ app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', 587))
 app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS', 'True') == 'True'
 app.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL', 'False') == 'True'
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.getenv('qcilofpzrnjugdro')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 
 mail = Mail(app)
 
